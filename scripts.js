1947e9d42b921f401686713c051fa8705a94c88e
Denis Griffis <denisrgriffis@gmail.com>
2016-01-08 13:15:51 -0500

    First draft of website

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function darken() {
+    $(this).addClass('dark');
+}
+function lighten() {
+    $(this).removeClass('dark');
+}
+
+$(document).ready(function() {
+    $('#links a.link').mouseover(darken);
+    $('#links a.link').mouseout(lighten);
+    $('.resources a').mouseover(darken);
+    $('.resources a').mouseout(lighten);
+
+    $('.resources').append('<div class="clearer"></div>');
+})
